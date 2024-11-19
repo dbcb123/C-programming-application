d32d954bd3753b1e268f6ed5cb393cdceeca2d65
dbcb123 <dbcb123@naver.com>
2024-11-19 23:08:59 +0900

    week12 exam

---
+++ week12/week12_exam2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "calculator.h"
+
+int main() {
+    printf("%d\n", add(10, 20));
+    printf("%d\n", sub(10, 20));
+    printf("%d\n", multi(10, 20));
+    printf("%d\n", printAdd(10, 20));
+    
+    return 0;
+}
